7fa3d9fe8212ff13a404e1649e6f31a58f637759
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-27 12:26:14 -0500

    se agrego prubas al component de pins

--- src/app/components/pins/pins.component.spec.ts
+++ src/app/components/pins/pins.component.spec.ts
@@ -1,14 +1,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { PINS } from 'src/app/services/mocks/pins';
 import { PinsComponent } from './pins.component';
+import { RepositoryService } from 'src/app/services/repository.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PinsService } from './pins.service';
+import { Subject, of } from 'rxjs';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
-describe('PinsComponent', () => {
+class RepositoryServiceStub {
+  observer = new Subject();
+
+  getPins() {
+    return this.observer;
+  }
+  savePins() {
+    return this.observer.next(JSON.parse(JSON.stringify(PINS)));
+  }
+  updatePin() {
+    return of(true);
+  }
+
+}
+class MatSnackBarStub {
+  open(){}
+}
+class PinsServiceStub {
+  observer = new Subject();
+  $actionObserver = this.observer.asObservable();
+  public resolve(action){
+    return this.observer.next(action);
+  }
+}
+fdescribe('PinsComponent', () => {
   let component: PinsComponent;
   let fixture: ComponentFixture<PinsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PinsComponent ]
+      declarations: [ PinsComponent ],
+      providers: [
+        {provide: RepositoryService, useClass: RepositoryServiceStub },
+        {provide: MatSnackBar, useClass: MatSnackBarStub },
+        {provide: PinsService, useClass: PinsServiceStub }
+      ],
+      imports: [ReactiveFormsModule],
+      schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -22,4 +59,30 @@ describe('PinsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('When new page is open', () => {
+    const open = spyOn(window, 'open');
+    component.openUrl('https://platzi.com');
+
+    expect(open).toHaveBeenCalledWith('https://platzi.com','_blank');
+  })
+
+  it('When update progress', () => {
+    component.pins = PINS;
+    const pin = PINS[0];
+    const updatePin = spyOn((<any>component).repository, 'updatePin').and.returnValue(of(true));
+    const open = spyOn((<any>component).snackBar, 'open');
+    const pinService = TestBed.get(PinsService);
+    pinService.resolve('save');
+
+    expect(open).toHaveBeenCalled();
+    expect(updatePin).toHaveBeenCalledWith(pin._id,{
+      title: pin.title,
+      author: pin.author,
+      description: pin.description,
+      percentage: pin.percentage,
+      tags: pin.tags,
+      assets: pin.assets
+    });
+  })
 });
